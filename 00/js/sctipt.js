9ce8919d9473c797ce139d6081b0f1fd37706a8e
PinkDeer <bunman@mail.ru>
2018-05-21 23:07:22 +0300

    Update 00

--- 00/js/sctipt.js
+++ 00/js/sctipt.js
@@ -1,6 +1,9 @@
-var text = document.querySelector('#text');
+var text = document.querySelector('#text'); // получить элемент по id
 
-text.onclick = function () {
+text.onclick = function () { //событие
   //text.style.color = 'red';
-  text.innerHTML = 'yeah';
+  text.innerHTML = 'yeah'; // свойства
+  text.className = 'test';
+  text.style.color = '#f00';
+  text.style.fontSize = '30px';
 }
